c369f34dc66389e94b8fe2143a8f818be9ac2034
Pierre Kuhnmunch <pierrekuh@yahoo.fr>
2021-02-11 07:44:17 +0100

    creation fichier scrap

--- app.py
+++ app.py
@@ -1,12 +1,17 @@
 from flask import Flask
 from flask import render_template
+from datetime import time
 
 app = Flask(__name__)
 
-@app.route('/hello/')
-@app.route('/hello/<name>')
-def hello(name=None):
-    return render_template('hello.html', name=name)
+
+@app.route("/simple_chart")
+def chart():
+    legend = 'Monthly Data'
+    labels = ["January", "February", "March", "April", "May", "June", "July", "August"]
+    values = [10, 9, 8, 7, 6, 4, 7, 8]
+    return render_template('chart.html', values=values, labels=labels, legend=legend)
 
 
-app.run(debug=True)
+if __name__ == "__main__":
+    app.run(debug=True)

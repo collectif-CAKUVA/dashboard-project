fff21d0cc69c929d5f74a1ce328273c6ab39178b
clement <clementcamara@gmail.com>
2021-02-15 18:28:58 +0100

    maj 2 clement

--- scrapper_js.py
+++ scrapper_js.py
@@ -1,13 +1,22 @@
-from scraper_api import ScraperAPIClient
-from config import *
-client = ScraperAPIClient({token})
-result = client.get(url={url}, render=True).text
-print(result)
+import requests
+import re
+from bs4 import BeautifulSoup
 
-# Scrapy users can simply replace the urls in their start_urls and parse function
-# Note for Scrapy, you should not use DOWNLOAD_DELAY and
-# RANDOMIZE_DOWNLOAD_DELAY, these will lower your concurrency and are not
-# needed with our API
-# ...other scrapy setup code
+# url = 'https://www.pmu.fr/turf/10022021/R1/C1'
+url2 = 'https://www.turf-fr.com/courses-pmu/pronostics/presse/r1-prix-de-grasse-229363'
+response = requests.get(url2)
+soup = BeautifulSoup(response.text, 'html.parser')
 
+#arrivée
+#arrivées = soup.select('strong')
+#for e in arrivées :
+#    print(e)
 
+def scrap_pronos_journaux():
+    pronos = soup.find_all("data", id="valeur")
+    journaux = soup.find_all("data",{"class":"pull-left balise_h4"}, id="nom")
+
+    list = [{ 'magasine':b.text.strip(),'prono':a.text.strip()} for a,b in zip(pronos, journaux)]
+    print(list)
+
+scrap_pronos_journaux()
